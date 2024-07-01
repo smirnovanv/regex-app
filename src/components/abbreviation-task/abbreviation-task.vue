1973f9a40e1220b38414cc9e3252c7cb94a2aaaf
Nataly <smirnovanv89@gmail.com>
2024-07-01 03:08:42 +0300

    fix: fix solution

--- src/components/abbreviation-task/abbreviation-task.vue
+++ src/components/abbreviation-task/abbreviation-task.vue
@@ -12,7 +12,7 @@ const handleClick = (value: string) => {
 
 const taskText = 'Заменить в тексте все аббревиатуры HTML, CSS, JS. Если эти сокращения написаны строчными — заменить на прописные. Важно, чтобы выражение не совпадало на расширениях файлов.';
 const exampleData = 'Одностраничное приложение — это приложение, которое состоит из единственной страницы и прочих ресурсов (таких как HTML, js и CSS), необходимых для успешной работы. Запуск приложения осуществляется из единственного файла index.html в корневой папке.';
-const solutionText = 'const insertAbbr = (str: string) => {return str.replace(/[^\.]\b(css|html|js)\b/gi, (s, $1) => {const shortName = $1.toUpperCase(); return ` <abbr title="${Abbr[shortName]}">${shortName}</abbr>`})};';
+const solutionText = 'см. код';
 
 </script>
 
